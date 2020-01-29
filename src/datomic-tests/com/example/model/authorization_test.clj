6cae663f2dc85513a95ffaa6d750702ce0bbcf75
Tony Kay <tony.kay@gmail.com>
2020-01-29 09:51:53 -0800

    Started adding CLJC numeric and date time support for full-stack data type support.

--- src/datomic-tests/com/example/model/authorization_test.clj
+++ src/datomic-tests/com/example/model/authorization_test.clj
@@ -24,8 +24,15 @@
           (::auth/provider actual) => :local
           "Returns the account holder's first name"
           (:account/name actual) => "Tony")))
+    (component "Bad username"
+      (let [actual (exauth/login! env {:username "tny@eample.com" :password "crap"})]
+        (assertions
+          "Returns an auth failure indicator"
+          (::auth/status actual) => :failed
+          "Returns the auth provider's identity"
+          (::auth/provider actual) => :local)))
     (component "Invalid credentials"
-      (let [actual (exauth/login! env {:username "tony" :password "crap"})]
+      (let [actual (exauth/login! env {:username "tony@example.com" :password "crap"})]
         (assertions
           "Returns an auth failure indicator"
           (::auth/status actual) => :failed
