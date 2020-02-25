56f77a8044836a00e9a8464746512468ffd8b349
Tony Kay <tony.kay@gmail.com>
2020-02-24 16:51:39 -0800

    changed signature of form middleware function.

--- src/shared/com/example/components/save_middleware.clj
+++ src/shared/com/example/components/save_middleware.clj
@@ -1,12 +1,9 @@
 (ns com.example.components.save-middleware
   (:require
-    [clojure.pprint :refer [pprint]]
-    [edn-query-language.core :as eql]
     [com.fulcrologic.rad.middleware.save-middleware :as r.s.middleware]
     [com.fulcrologic.rad.database-adapters.datomic :as datomic]
     [com.example.components.datomic :refer [datomic-connections]]
     [com.fulcrologic.rad.blob :as blob]
-    [mount.core :refer [defstate]]
     [com.example.model :as model]))
 
 (def middleware
