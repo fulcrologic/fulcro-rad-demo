28b8c2dd3cd9f9be66a621627d079bb19bd5e53b
Tony Kay <tony.kay@gmail.com>
2022-11-18 09:58:37 -0500

    Upgraded dependencies, and added new HTML5 default route routing option.

--- src/shared/com/example/client.cljs
+++ src/shared/com/example/client.cljs
@@ -2,23 +2,21 @@
   (:require
     [com.example.ui :as ui :refer [Root]]
     [com.example.ui.login-dialog :refer [LoginForm]]
+    [com.fulcrologic.fulcro.algorithms.timbre-support :refer [console-appender prefix-output-fn]]
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp]
     [com.fulcrologic.fulcro.mutations :as m]
+    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
     [com.fulcrologic.rad.application :as rad-app]
-    [com.fulcrologic.rad.locale :as r.locale]
-    [com.fulcrologic.rad.report :as report]
     [com.fulcrologic.rad.authorization :as auth]
     [com.fulcrologic.rad.rendering.semantic-ui.semantic-ui-controls :as sui]
-    [com.fulcrologic.fulcro.algorithms.timbre-support :refer [console-appender prefix-output-fn]]
-    [taoensso.timbre :as log]
-    [taoensso.tufte :as tufte :refer [profile]]
-    [com.fulcrologic.rad.type-support.date-time :as datetime]
-    [com.fulcrologic.fulcro.algorithms.tx-processing.synchronous-tx-processing :as stx]
-    [com.fulcrologic.rad.routing.html5-history :as hist5 :refer [html5-history]]
-    [com.fulcrologic.rad.routing.history :as history]
+    [com.fulcrologic.rad.report :as report]
     [com.fulcrologic.rad.routing :as routing]
-    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]))
+    [com.fulcrologic.rad.routing.history :as history]
+    [com.fulcrologic.rad.routing.html5-history :as hist5 :refer [new-html5-history]]
+    [com.fulcrologic.rad.type-support.date-time :as datetime]
+    [taoensso.timbre :as log]
+    [taoensso.tufte :as tufte :refer [profile]]))
 
 (defonce stats-accumulator
   (tufte/add-accumulating-handler! {:ns-pattern "*"}))
@@ -56,7 +54,8 @@
   (dr/initialize! app)
   (setup-RAD app)
   (dr/change-route! app ["landing-page"])
-  (history/install-route-history! app (html5-history))
+  (history/install-route-history! app (new-html5-history {:app           app
+                                                          :default-route {:route ["landing-page"]}}))
   (auth/start! app [LoginForm] {:after-session-check `fix-route})
   (app/mount! app Root "app" {:initialize-state? false}))
 
